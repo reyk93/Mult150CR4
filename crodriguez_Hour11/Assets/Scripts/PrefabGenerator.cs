693a55ed7b5a765bef38a9db7a811a31e27fd117
reyk93 <citlalirgonz@gmail.com>
2025-11-17 13:24:44 -0800

    For Hour 11 project (after Point 11), I added a  while loop to create 10 street lamps in the scene hour 11 and used vector3 functions to make them appear in a line along the X axis. I also removed the rigidbody component of the original prefab since it was making the cloned lamps to jump or shake in the scene.

--- crodriguez_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ crodriguez_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,11 +6,42 @@ public class PrefabGenerator : MonoBehaviour
 {
 
     public GameObject prefab;
+    
+    int prefabCount = 1; // these int create the 10 street lamps in the scene //
+    int sum = 0;
+
+    /*public Vector3 spawnRange;*/
+
+    public float spacingOfPrefab =3f;
+    public Vector3 startPoint = Vector3.zero;  // this is the start position where the lamps will appear first//
+    public Vector3 lineDirection = Vector3.right; // it will make the lamps spand in the x axis to the right//
+
+    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         
+
+        while (prefabCount <= 10)   // this is reading the int information and creating the 10 prefabs//
+        {
+            sum += prefabCount;
+            prefabCount++;
+
+            // Vector3 spawnPositionA = new Vector3(Random.Range(-spawnRange.x,spawnRange.x), 0f,-6f);    this works with the  public Vector3 spawnRange above//
+
+            Vector3 spawnPositionA = startPoint + (lineDirection.normalized * spacingOfPrefab * prefabCount);  // normalized makes sure the vecotr has a magnitude of 1 so spacingOfPrefab work and trnaslate to distance//
+            Instantiate(prefab,spawnPositionA , transform.rotation);
+
+            
+
+
+        }
+
+       
+
     }
 
     // Update is called once per frame
@@ -25,4 +56,8 @@ public class PrefabGenerator : MonoBehaviour
             Instantiate(prefab, transform.position, transform.rotation);
         }
     }
+
+
+
+
 }
