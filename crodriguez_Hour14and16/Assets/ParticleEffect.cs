5c08a36ce60cf40d1c17a58f75856ff1b65a795a
reyk93 <citlalirgonz@gmail.com>
2025-11-23 22:53:04 -0800

    For Hour 16, I completed the TIY and created a new scene "Hour16". In the scene I added a new particle system and changed some of its properties like color, size, way they fall, speed, and so on.

---
+++ crodriguez_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,3 @@
+/* Modified parameters: changed the color in the Start Color using a gradient editor,  changed shape from cone to donut (Radius: 3.7), and checked collision with type : World.*/
+/*The new changes also include size by speed where I used a curves in the X,Y,Z axis to change the velocity of the particles*/
+/* There is also a change in Rotation by speed using Angular Velocity of 45 to 90* and a change in Gravity Modifier using a curve and Z value of 3*/
